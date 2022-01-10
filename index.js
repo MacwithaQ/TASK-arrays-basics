25747bd23b3f14b2b84da4b7048d9515b077a18f
MacwithaQ <maqallaf@gmail.com>
2022-01-10 16:46:56 +0300

    completed the tasks with no issues

---
+++ index.js
@@ -0,0 +1,36 @@
+const emptyArray=[];
+
+const hobbies=[
+    `musicProduction`,
+    `fishing`,
+    `coding`,
+    `videoEditing`,
+    `cooking`,
+    `reading`
+];
+
+const numbers=[
+    7,
+    9,
+    3,
+    1,
+    6
+];
+
+console.log(numbers[1]);
+
+console.log(hobbies[hobbies.length-1]);
+
+hobbies.push(`skiing`);
+
+console.log(hobbies[hobbies.length-1]);
+
+console.log(hobbies.length);
+
+hobbies.splice(hobbies.length-2);
+
+console.log(hobbies.length);
+
+console.log(hobbies);
+
+hobbies.length=0;
